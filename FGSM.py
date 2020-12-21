31a4f38df3fb0fa89983f70c4c126a5be1ce5c98
pyx <wenciline00@163.com>
2020-12-21 14:36:56 +0800

    prepare image

--- FGSM.py
+++ FGSM.py
@@ -13,3 +13,52 @@ import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
 
+#set args
+eps = 0.007
+use_cuda = True
+
+#prepare data
+class_idx = json.load(open("./data/imagenet_class_index.json"))
+idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
+transform = transforms.Compose([
+    transforms.Resize((299, 299)),
+    transforms.ToTensor(), # ToTensor : [0, 255] -> [0, 1]
+])
+
+def image_folder_custom_label(root, transform, custom_label) :
+    
+    # custom_label
+    # type : List
+    # index -> label
+    # ex) ['tench', 'goldfish', 'great_white_shark', 'tiger_shark']
+    
+    old_data = dsets.ImageFolder(root = root, transform = transform)
+    old_classes = old_data.classes
+
+    label2idx = {}
+    
+    for i, item in enumerate(idx2label) :
+        label2idx[item] = i
+    
+    new_data = dsets.ImageFolder(root = root, transform = transform, 
+                                 target_transform = lambda x : custom_label.index(old_classes[x]))
+    new_data.classes = idx2label
+    new_data.class_to_idx = label2idx
+
+    return new_data
+
+normal_data = image_folder_custom_label(root = './data/', transform = transform, custom_label = idx2label)
+normal_loader = Data.DataLoader(normal_data, batch_size=1, shuffle=False)
+
+def imshow(img, title):
+    npimg = img.numpy()
+    fig = plt.figure(figsize = (5, 15))
+    plt.imshow(np.transpose(npimg,(1,2,0)))
+    plt.title(title)
+    plt.show()
+
+normal_iter = iter(normal_loader)  #生成迭代器
+images, labels = normal_iter.next()
+
+print("True Image & True Label")
+imshow(torchvision.utils.make_grid(images, normalize=True), [normal_data.classes[i] for i in labels])
